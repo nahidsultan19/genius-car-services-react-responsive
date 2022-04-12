a66c6836fe209aed86dbc5465cf5cd23f699bb8f
nahidsultan19 <nahidsultan38@gmail.com>
2022-04-13 00:17:38 +0600

    setup evvironment variable for firebase.

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,19 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAjm8Lx8mvY_VsqhZwQT6set0eyt7tidyc",
-    authDomain: "genius-car-services-e5c72.firebaseapp.com",
-    projectId: "genius-car-services-e5c72",
-    storageBucket: "genius-car-services-e5c72.appspot.com",
-    messagingSenderId: "758942654882",
-    appId: "1:758942654882:web:046c30443eb70a443ed2fb"
+    /*   apiKey: "AIzaSyAjm8Lx8mvY_VsqhZwQT6set0eyt7tidyc",
+      authDomain: "genius-car-services-e5c72.firebaseapp.com",
+      projectId: "genius-car-services-e5c72",
+      storageBucket: "genius-car-services-e5c72.appspot.com",
+      messagingSenderId: "758942654882",
+      appId: "1:758942654882:web:046c30443eb70a443ed2fb" */
+
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
