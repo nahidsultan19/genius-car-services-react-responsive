4c65d86eeb71c7543b97b21774cf2c73ca86fa02
nahidsultan19 <nahidsultan38@gmail.com>
2022-04-13 01:02:40 +0600

    added require auth component.

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const Login = () => {
@@ -9,11 +9,14 @@ const Login = () => {
     const passwordRef = useRef('');
     const navigate = useNavigate()
 
+    const location = useLocation();
+    let from = location.state?.from?.pathname || '/';
+
     const [signInWithEmailAndPassword, user, loading, error,] = useSignInWithEmailAndPassword(auth);
 
 
     if (user) {
-        navigate('/home');
+        navigate(from, { replace: true });
     }
 
     const handleSubmit = event => {
